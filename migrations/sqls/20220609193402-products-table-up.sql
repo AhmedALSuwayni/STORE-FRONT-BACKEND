9149f8b58e7c36d297129e13cef8c49264b75b96
AhmedALSuwayni <ahmedsuwayni@gmail.com>
2022-07-11 16:04:44 +0300

    dump data

--- migrations/sqls/20220609193402-products-table-up.sql
+++ migrations/sqls/20220609193402-products-table-up.sql
@@ -4,3 +4,11 @@ CREATE TABLE products (
     price integer NOT NULL,
     category VARCHAR(18),
     urlImage text);
+    INSERT INTO public.products(name, price, category, "urlImage") VALUES ('Table', 200, 'wood', 'http://localhost:5000/images/woodtable.jpg');
+    INSERT INTO public.products(name, price, category, "urlImage") VALUES ('Chair', 100, 'wood', 'http://localhost:5000/images/woodchair.jpg');
+    INSERT INTO public.products(name, price, category, "urlImage") VALUES ('Spoon', 10, 'wood', 'http://localhost:5000/images/woodspoon.jpg');
+    INSERT INTO public.products(name, price, category, "urlImage") VALUES ('Fork', 11, 'wood', 'http://localhost:5000/images/woodfork.jpg');
+    INSERT INTO public.products(name, price, category, "urlImage") VALUES ('Table', 400, 'iron', 'http://localhost:5000/images/irontable.jpg');
+    INSERT INTO public.products(name, price, category, "urlImage") VALUES ('Chair', 200, 'iron', 'http://localhost:5000/images/ironchair.jpg');
+    INSERT INTO public.products(name, price, category, "urlImage") VALUES ('Spoon', 20, 'iron', 'http://localhost:5000/images/ironspoon.jpg');
+    INSERT INTO public.products(name, price, category, "urlImage") VALUES ('Fork', 22, 'iron', 'http://localhost:5000/images/ironfork.jpg');
